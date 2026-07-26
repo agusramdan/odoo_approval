c8012fc9580e349ec7d64f870fae0f8938889747
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2026-07-26 16:49:47 +0700

    amr_approval_hr 13.0.1.0.0

---
+++ amr_approval_hr/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Approval HR",
+    'summary': "Configuration Approval Using HR Model",
+    'description': """
+    """,
+    'author': "Agus Muhammad Ramdan",
+    'website': "http://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'version': '13.0.1.0.0',
+    'depends': ['base', 'mail', 'hr', 'amr_approval'],
+    # always loaded
+    'data': [
+        'data/approval_responsible_data.xml',
+    ],
+}
